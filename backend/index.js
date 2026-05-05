e90e42bc77755ca5dc14abb9a6c96b3ed328e2e9
Gautam Kumar <g.k965069@gmail.com>
2026-05-06 03:45:49 +0530

    backend servere has been created

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res)=>{
+   res.send("welcome to the port 5000")
+})
+
+app.listen(port, ()=>{
+  console.log(`server is listing at ${port}`);
+})
