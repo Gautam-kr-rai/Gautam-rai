f298bb377d743e42e755d3cd6be01c2e9e4d4b28
Gautam Kumar <g.k965069@gmail.com>
2026-07-08 02:44:23 +0530

    db connect

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import express from 'express'
-
+import dotenv from "dotenv"
+import connectDB from './lib/dbConnection.js';
+import mongoose from 'mongoose';
+// import dns from "node:dns/promises";   
+// dns.setServers(["1.1.1.1", "1.0.0.1"]);  
+dotenv.config()
 
 const app = express();
 
 const port = 5000;
+
+app.use(express.json());
+
 
 app.get("/", (req, res)=>{
    res.send("welcome to the port 5000")
 })
 
 app.listen(port, ()=>{
+  connectDB()
   console.log(`server is listing at ${port}`);
 })
